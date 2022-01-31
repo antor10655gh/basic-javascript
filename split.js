ef3bc0c3c313b223aabf67cf94d2c6d16569a4e7
antor10655gh <creativefreelancerantor@gmail.com>
2022-01-31 13:09:00 +0600

    add split.js

---
+++ split.js
@@ -0,0 +1,7 @@
+// use of split(""), split(","), split("  "), split("|")---------
+let text = "I Love Bangladesh";
+
+console.log(text.split(""));
+console.log(text.split(","));
+console.log(text.split("  "));
+console.log(text.split("|"));
--- stringMethod.js
+++ stringMethod.js
@@ -2,3 +2,4 @@ let myName = "Md AM Antor";
 let sliceName = myName.slice(6, 12);
 
 console.log(sliceName.toUpperCase());
+
