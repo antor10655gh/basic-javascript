6040ec21f200af526e7186ced2fa0f1b6009f15d
antor10655gh <creativefreelancerantor@gmail.com>
2022-02-01 00:33:41 +0600

    add fiboNacci.js again

--- fiboNacci.js
+++ fiboNacci.js
@@ -1,6 +1,10 @@
-let fiboNacci = [0, 1];
-for(let i = 2; i <= 10; i++){
-    fiboNacci[i] = fiboNacci[i-1] + fiboNacci[i-2];
+function fiboNacciSeq(fiboNacci){
+    for(let i = 2; i <= 10; i++){
+        fiboNacci[i] = fiboNacci[i-1] + fiboNacci[i-2];
+    }
+    return fiboNacci;
 }
 
-console.log(fiboNacci);
+let fiboNacci = [0, 1];
+let finalFiboNacci = fiboNacciSeq(fiboNacci);
+console.log(finalFiboNacci);
