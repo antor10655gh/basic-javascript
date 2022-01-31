4941235badc4908879b2a01d5bf1ef5d51590cc9
antor10655gh <creativefreelancerantor@gmail.com>
2022-01-31 19:05:10 +0600

    add mathObject.js

---
+++ mathObject.js
@@ -0,0 +1,15 @@
+let number  = 4.5576;
+let number2 = 36;
+let number3 = -9;
+let numbers = [2, 3, 5, 56, 34, 22, 20];
+
+console.log(`Round value is: ${Math.round(number)}`);
+console.log(`Ceil value is: ${Math.ceil(number)}`);
+console.log(`Floor value is: ${Math.floor(number)}`);
+console.log(`Root value is: ${Math.sqrt(number2)}`);
+console.log(`Absolute value is: ${Math.abs(number3)}`);
+console.log(`Power value is: ${Math.pow(number3, 2)}`);
+console.log(`Max value is: ${Math.max(...numbers)}`);
+console.log(`Min value is: ${Math.min(...numbers)}`);
+
+
